c6547a5d22b2deb73dbb93a557886c1ed7375547
sedoy113 <sedoy113@yandex.ru>
2021-08-23 23:37:37 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,5 @@ const status = { healthy: 51, wounded: 15, critical: 0 };
 export default function streakOfLife(player) {
   return Object.keys(status).find((name) => status[name] <= player.health);
 }
+
+//
